850552457474665cfe1744ee968a6b522210d2e8
Xu <moonshadow-xu@hotmail.com>
2018-04-26 15:06:18 +0800

    添加注释

--- nnet/ann_bp.cpp
+++ nnet/ann_bp.cpp
@@ -3,76 +3,84 @@
 #include "ann_bp.h"
 #include <math.h>
 
+using std::cout;
+using std::endl;
+
+// 定义构造函数
 Ann_bp::Ann_bp(int _SampleN, int nNIL, int nNOL, const int nNHL, float _sR) :
 	SampleCount(_SampleN), numNodesInputLayer(nNIL), numNodesOutputLayer(nNOL),
 	numNodesHiddenLayer(nNHL), studyRate(_sR)
 {
 
 	//创建权值空间,并初始化
-	srand(time(NULL));
-	weights = new double**[2];
-	weights[0] = new double *[numNodesInputLayer];
-	for (int i = 0; i < numNodesInputLayer; ++i) {
+	srand(time(NULL));    // 以时间为内核设计一个随机数种子
+	weights = new double**[2];  // 定义权值矩阵
+	weights[0] = new double *[numNodesInputLayer]; // 输入层的权值矩阵
+	for (int i = 0; i < numNodesInputLayer; ++i) 
+	{
 		weights[0][i] = new double[numNodesHiddenLayer];
-		for (int j = 0; j <numNodesHiddenLayer; ++j) {
-			weights[0][i][j] = (rand() % (2000) / 1000.0 - 1); //-1到1之间
+		for (int j = 0; j <numNodesHiddenLayer; ++j) 
+		{
+			weights[0][i][j] = (rand() % (2000) / 1000.0 - 1); // -1到1之间
 		}
 	}
-	weights[1] = new double *[numNodesHiddenLayer];
-	for (int i = 0; i < numNodesHiddenLayer; ++i) {
+	weights[1] = new double *[numNodesHiddenLayer];// 隐藏层的权值矩阵
+	for (int i = 0; i < numNodesHiddenLayer; ++i) 
+	{
 		weights[1][i] = new double[numNodesOutputLayer];
-		for (int j = 0; j < numNodesOutputLayer; ++j) {
+		for (int j = 0; j < numNodesOutputLayer; ++j) 
+		{
 			weights[1][i][j] = (rand() % (2000) / 1000.0 - 1); //-1到1之间
 		}
 	}
 
 	//创建偏置空间，并初始化
 	bias = new double *[2];
-	bias[0] = new double[numNodesHiddenLayer];
-	for (int i = 0; i < numNodesHiddenLayer; ++i) {
+	bias[0] = new double[numNodesHiddenLayer];   //隐藏层的偏置值
+	for (int i = 0; i < numNodesHiddenLayer; ++i) 
+	{
 		bias[0][i] = (rand() % (2000) / 1000.0 - 1); //-1到1之间
 	}
-	bias[1] = new double[numNodesOutputLayer];
-	for (int i = 0; i < numNodesOutputLayer; ++i) {
+	bias[1] = new double[numNodesOutputLayer];  // 输出层的偏置值
+	for (int i = 0; i < numNodesOutputLayer; ++i) 
+	{
 		bias[1][i] = (rand() % (2000) / 1000.0 - 1); //-1到1之间
 	}
 
-	//创建隐藏层各结点的输出值空间
+	// 创建隐藏层各结点的输出值空间
 	hidenLayerOutput = new double[numNodesHiddenLayer];
-	//创建输出层各结点的输出值空间
+	// 创建输出层各结点的输出值空间
 	outputLayerOutput = new double[numNodesOutputLayer];
 
-	//创建所有样本的权值更新量存储空间
+	// 创建所有样本的权值更新量存储空间
 	allDeltaWeights = new double ***[_SampleN];
-	for (int k = 0; k < _SampleN; ++k) {
+	for (int k = 0; k < _SampleN; ++k) 
+	{
 		allDeltaWeights[k] = new double**[2];
 		allDeltaWeights[k][0] = new double *[numNodesInputLayer];
-		for (int i = 0; i < numNodesInputLayer; ++i) {
+		for (int i = 0; i < numNodesInputLayer; ++i) 
 			allDeltaWeights[k][0][i] = new double[numNodesHiddenLayer];
-		}
 		allDeltaWeights[k][1] = new double *[numNodesHiddenLayer];
-		for (int i = 0; i < numNodesHiddenLayer; ++i) {
+		for (int i = 0; i < numNodesHiddenLayer; ++i) 
 			allDeltaWeights[k][1][i] = new double[numNodesOutputLayer];
-		}
 	}
 
-	//创建所有样本的偏置更新量存储空间
+	// 创建所有样本的偏置更新量存储空间
 	allDeltaBias = new double **[_SampleN];
-	for (int k = 0; k < _SampleN; ++k) {
+	for (int k = 0; k < _SampleN; ++k) 
+	{
 		allDeltaBias[k] = new double *[2];
 		allDeltaBias[k][0] = new double[numNodesHiddenLayer];
 		allDeltaBias[k][1] = new double[numNodesOutputLayer];
 	}
 
-	//创建存储所有样本的输出层输出空间
+	// 创建存储所有样本的输出层输出空间
 	outputMat = new double*[_SampleN];
-	for (int k = 0; k < _SampleN; ++k) {
+	for (int k = 0; k < _SampleN; ++k) 
 		outputMat[k] = new double[numNodesOutputLayer];
-	}
-
-
 }
 
+// 定义析构函数
 Ann_bp::~Ann_bp()
 {
 	//释放权值空间
@@ -90,7 +98,8 @@ Ann_bp::~Ann_bp()
 	delete[] bias;
 
 	//释放所有样本的权值更新量存储空间
-	for (int k = 0; k < SampleCount; ++k) {
+	for (int k = 0; k < SampleCount; ++k) 
+	{
 		for (int i = 0; i < numNodesInputLayer; ++i)
 			delete[] allDeltaWeights[k][0][i];
 		for (int i = 1; i < numNodesHiddenLayer; ++i)
@@ -102,7 +111,8 @@ Ann_bp::~Ann_bp()
 	delete[] allDeltaWeights;
 
 	//释放所有样本的偏置更新量存储空间
-	for (int k = 0; k < SampleCount; ++k) {
+	for (int k = 0; k < SampleCount; ++k) 
+	{
 		for (int i = 0; i < 2; ++i)
 			delete[] allDeltaBias[k][i];
 		delete[] allDeltaBias[k];
@@ -118,61 +128,74 @@ Ann_bp::~Ann_bp()
 
 void Ann_bp::train(const int _sampleNum, float** _trainMat, int** _labelMat)
 {
-	double thre = 1e-4;
-	for (int i = 0; i < _sampleNum; ++i) {
+	double thre = 1e-4;  // 定义一个阈值，表示训练的精度
+	for (int i = 0; i < _sampleNum; ++i) 
+	{
 		train_vec(_trainMat[i], _labelMat[i], i);
 	}
 	int tt = 0;
-	while (isNotConver(_sampleNum, _labelMat, thre) && tt<100000) {
+	int iter = 100000;   // 迭代次数
+	while (isNotConver(_sampleNum, _labelMat, thre) && tt< iter) 
+	{
 		tt++;
 		//调整权值
-		for (int index = 0; index < _sampleNum; ++index) {
-			for (int i = 0; i < numNodesInputLayer; ++i) {
-				for (int j = 0; j < numNodesHiddenLayer; ++j) {
+		for (int index = 0; index < _sampleNum; ++index) 
+		{
+			for (int i = 0; i < numNodesInputLayer; ++i) 
+			{
+				for (int j = 0; j < numNodesHiddenLayer; ++j) 
+				{
 					weights[0][i][j] -= studyRate * allDeltaWeights[index][0][i][j];
 				}
 			}
-			for (int i = 0; i < numNodesHiddenLayer; ++i) {
-				for (int j = 0; j < numNodesOutputLayer; ++j) {
+			for (int i = 0; i < numNodesHiddenLayer; ++i) 
+			{
+				for (int j = 0; j < numNodesOutputLayer; ++j) 
+				{
 					weights[1][i][j] -= studyRate * allDeltaWeights[index][1][i][j];
 				}
 			}
 		}
 
-		for (int index = 0; index < _sampleNum; ++index) {
-			for (int i = 0; i < numNodesHiddenLayer; ++i) {
+		for (int index = 0; index < _sampleNum; ++index) 
+		{
+			for (int i = 0; i < numNodesHiddenLayer; ++i) 
+			{
 				bias[0][i] -= studyRate * allDeltaBias[index][0][i];
 			}
-			for (int i = 0; i < numNodesOutputLayer; ++i) {
+			for (int i = 0; i < numNodesOutputLayer; ++i) 
+			{
 				bias[1][i] -= studyRate * allDeltaBias[index][1][i];
 			}
 		}
 
-		for (int i = 0; i < _sampleNum; ++i) {
+		for (int i = 0; i < _sampleNum; ++i) 
+		{
 			train_vec(_trainMat[i], _labelMat[i], i);
 		}
 	}
 
-	printf("训练权值和偏置成功了！\n");
+	cout << "权值和偏置训练成功了！" << endl;
 }
 
 void Ann_bp::train_vec(const float* _trainVec, const int* _labelVec, int index)
 {
 	//计算各隐藏层结点的输出
-	for (int i = 0; i < numNodesHiddenLayer; ++i) {
+	for (int i = 0; i < numNodesHiddenLayer; ++i) 
+	{
 		double z = 0.0;
-		for (int j = 0; j < numNodesInputLayer; ++j) {
+		for (int j = 0; j < numNodesInputLayer; ++j) 
 			z += _trainVec[j] * weights[0][j][i];
-		}
 		z += bias[0][i];
 		hidenLayerOutput[i] = sigmoid(z);
-
 	}
 
 	//计算输出层结点的输出值
-	for (int i = 0; i < numNodesOutputLayer; ++i) {
+	for (int i = 0; i < numNodesOutputLayer; ++i) 
+	{
 		double z = 0.0;
-		for (int j = 0; j < numNodesHiddenLayer; ++j) {
+		for (int j = 0; j < numNodesHiddenLayer; ++j) 
+		{
 			z += hidenLayerOutput[j] * weights[1][j][i];
 		}
 		z += bias[1][i];
@@ -182,20 +205,25 @@ void Ann_bp::train_vec(const float* _trainVec, const int* _labelVec, int index)
 
 	//计算偏置及权重更新量，但不更新
 
-	for (int j = 0; j <numNodesOutputLayer; ++j) {
+	for (int j = 0; j <numNodesOutputLayer; ++j) 
+	{
 		allDeltaBias[index][1][j] = (-0.1)*(_labelVec[j] - outputLayerOutput[j])*outputLayerOutput[j]
 			* (1 - outputLayerOutput[j]);
-		for (int i = 0; i < numNodesHiddenLayer; ++i) {
+		for (int i = 0; i < numNodesHiddenLayer; ++i) 
+		{
 			allDeltaWeights[index][1][i][j] = allDeltaBias[index][1][j] * hidenLayerOutput[i];
 		}
 	}
-	for (int j = 0; j < numNodesHiddenLayer; ++j) {
+	for (int j = 0; j < numNodesHiddenLayer; ++j) 
+	{
 		double z = 0.0;
-		for (int k = 0; k < numNodesOutputLayer; ++k) {
+		for (int k = 0; k < numNodesOutputLayer; ++k) 
+		{
 			z += weights[1][j][k] * allDeltaBias[index][1][k];
 		}
 		allDeltaBias[index][0][j] = z * hidenLayerOutput[j] * (1 - hidenLayerOutput[j]);
-		for (int i = 0; i < numNodesInputLayer; ++i) {
+		for (int i = 0; i < numNodesInputLayer; ++i) 
+		{
 			allDeltaWeights[index][0][i][j] = allDeltaBias[index][0][j] * _trainVec[i];
 		}
 	}
@@ -203,13 +231,14 @@ void Ann_bp::train_vec(const float* _trainVec, const int* _labelVec, int index)
 }
 
 
-bool Ann_bp::isNotConver(const int _sampleNum,
-	int** _labelMat, double _thresh)
+bool Ann_bp::isNotConver(const int _sampleNum,int** _labelMat, double _thresh)
 {
 	double lossFunc = 0.0;
-	for (int k = 0; k < _sampleNum; ++k) {
+	for (int k = 0; k < _sampleNum; ++k) 
+	{
 		double loss = 0.0;
-		for (int t = 0; t < numNodesOutputLayer; ++t) {
+		for (int t = 0; t < numNodesOutputLayer; ++t) 
+		{
 			loss += (outputMat[k][t] - _labelMat[k][t])*(outputMat[k][t] - _labelMat[k][t]);
 		}
 		lossFunc += (1.0 / 2)*loss;
@@ -258,20 +287,23 @@ void Ann_bp::predict(float* in, float* proba)
 	//Sleep(5000);
 
 	//计算各隐藏层结点的输出
-	for (int i = 0; i < numNodesHiddenLayer; ++i) {
+	for (int i = 0; i < numNodesHiddenLayer; ++i) 
+	{
 		double z = 0.0;
-		for (int j = 0; j < numNodesInputLayer; ++j) {
+		for (int j = 0; j < numNodesInputLayer; ++j) 
+		{
 			z += in[j] * weights[0][j][i];
 		}
 		z += bias[0][i];
 		hidenLayerOutput[i] = sigmoid(z);
-
 	}
 
 	//计算输出层结点的输出值
-	for (int i = 0; i < numNodesOutputLayer; ++i) {
+	for (int i = 0; i < numNodesOutputLayer; ++i) 
+	{
 		double z = 0.0;
-		for (int j = 0; j < numNodesHiddenLayer; ++j) {
+		for (int j = 0; j < numNodesHiddenLayer; ++j) 
+		{
 			z += hidenLayerOutput[j] * weights[1][j][i];
 		}
 		z += bias[1][i];
--- nnet/ann_bp.h
+++ nnet/ann_bp.h
@@ -1,5 +1,4 @@
 #pragma once
-//ann_bp.h//
 #ifndef _ANN_BP_H_
 #define _ANN_BP_H_
 
@@ -10,10 +9,18 @@
 #include <windows.h>
 #include <ctime>
 
+// 声明一个类
 class Ann_bp
 {
 	public:
+		/*
+		  explicit关键字用于修饰只有一个参数的类构造函数, 它的作用是表明该构造函数是显示的, 而非隐式的；
+		  explicit关键字只对有一个参数的类构造函数有效, 如果类构造函数参数大于或等于两个时, 是不会产生隐式转换的, explicit也就无效了；
+		  也有一个例外，当除了第一个参数以外的其他参数都有默认值的时候, explicit关键字依然有效。
+		*/
+		// 类构造函数
 		explicit Ann_bp(int _SampleN, int nNIL, int nNOL, const int nNHL, float _sR = 0.2);
+		// 类析构函数
 		~Ann_bp();
 
 		void train(int _sampleNum, float** _trainMat, int** _labelMat);
@@ -23,23 +30,21 @@ class Ann_bp
 		int numNodesInputLayer;
 		int numNodesOutputLayer;
 		int numNodesHiddenLayer;
-		int SampleCount;               //总的训练样本数
-		double ***weights;            //网络权值
-		double **bias;                 //网络偏置
-		float studyRate;               //学习速率
+		int SampleCount;               // 总的训练样本数
+		double ***weights;             // 网络权值
+		double **bias;                 // 网络偏置
+		float studyRate;               // 学习速率
 
-		double *hidenLayerOutput;     //隐藏层各结点的输出值
-		double *outputLayerOutput;     //输出层各结点的输出值
+		double *hidenLayerOutput;      // 隐藏层各结点的输出值
+		double *outputLayerOutput;     // 输出层各结点的输出值
 
-		double ***allDeltaBias;        //所有样本的偏置更新量
-		double ****allDeltaWeights;    //所有样本的权值更新量
-		double **outputMat;            //所有样本的输出层输出
+		double ***allDeltaBias;        // 所有样本的偏置更新量
+		double ****allDeltaWeights;    // 所有样本的权值更新量
+		double **outputMat;            // 所有样本的输出层输出
 
 		void train_vec(const float* _trainVec, const int* _labelVec, int index);
 		double sigmoid(double x) { return 1 / (1 + exp(-1 * x)); }
 		bool isNotConver(const int _sampleNum, int** _labelMat, double _thresh);
-		// bool Ann_bp::isNotConver(const int _sampleNum, int** _labelMat, double _thresh);
-
 };
 
 #endif
